aa628610b5fa2b9b5d25639b03bd90268d7f2df0
eshakeerthiga <eshakeerthigae@gmail.com>
2024-12-26 11:35:42 +0530

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,29 +13,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Employee Details'),
+          title: Text(
+            'Employee Details',
+            style: GoogleFonts.anton(fontSize: 25),
+          ),
           centerTitle: true,
         ),
         body: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(30.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const SizedBox(height: 10),
-              const Align(
+              Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
                   'Name: ',
-                  style: TextStyle(fontSize: 18),
+                  style: GoogleFonts.anton(fontSize: 18),
                 ),
               ),
               const SizedBox(height: 10),
               const TextField(
                 decoration: InputDecoration(
                   hintText: 'Enter Name',
-                  contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 12.0),
-                  border: OutlineInputBorder(), 
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.green,
+                    size: 18,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                  border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 16),
@@ -51,9 +58,13 @@ class MyApp extends StatelessWidget {
               const TextField(
                 decoration: InputDecoration(
                   hintText: 'Enter Email',
-                  contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 12.0),
-                  border: OutlineInputBorder(), 
+                  prefixIcon: Icon(
+                    Icons.email,
+                    size: 18,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                  border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 16),
@@ -68,9 +79,13 @@ class MyApp extends StatelessWidget {
               const TextField(
                 decoration: InputDecoration(
                   hintText: 'Enter Contact',
-                  contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 12.0),
-                  border: OutlineInputBorder(), 
+                  prefixIcon: Icon(
+                    Icons.phone,
+                    size: 18,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                  border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 16),
@@ -85,9 +100,13 @@ class MyApp extends StatelessWidget {
               const TextField(
                 decoration: InputDecoration(
                   hintText: 'Enter Department',
-                  contentPadding: EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 12.0),
-                  border: OutlineInputBorder(), 
+                  prefixIcon: Icon(
+                    Icons.card_membership,
+                    size: 18,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                  border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 20),
